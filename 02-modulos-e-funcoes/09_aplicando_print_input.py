3ebf04658091b57a5c2482b1822ea4261214ca06
Francisco Pereira <fepereiraadm@gmail.com>
2026-03-24 08:28:29 -0300

    Create 09_aplicando_print_input.py

---
+++ 02-modulos-e-funcoes/09_aplicando_print_input.py
@@ -0,0 +1,3 @@
+departamento = input("Digite o nome do departamento: ")
+responsavel = input("Digite o nome da pessoa responsável: ")
+print("O departamento de " + departamento + " é liderado por " + responsavel + ".")
